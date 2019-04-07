b60721b6172192fa68247375c546bc4f2a5f6a66
alexandmrwh <jsntwh@icloud.com>
2019-04-07 23:08:23 +0800

    add missing rate setting

--- disfa_split.py
+++ disfa_split.py
@@ -2,8 +2,8 @@ import numpy as np
 import os
 path_DISFA = '../DISFA_10aus/DISFA_face_crop_10aus/'
 path_DISFA_picked_label = '../DISFA_10aus/DISFA_face_crop_10aus/DISFA_picked_label/'
-path_result = '../DISFA_10aus/'
-stride = 2
+path_result = '../DISFA_10aus_missingrate0.25/'
+stride = 4 # missing rate = 0.25
 au_num = 10
 train_idx = [2,3,4,6,7,8,12,13,17,18,23,26,28,29,30,32]
 val_idx=[1,5,9,10,11,14,15,16,19,20,21,22,24,25,27,31]
@@ -27,16 +27,17 @@ def split(type):
             all_samples = session_label.readlines()
             sample_num = len(all_samples)
             aus = np.zeros(10)
-            for i in range(0, sample_num-stride):
+            for i in range(0, sample_num-stride+1, stride):
                 line1 = all_samples[i]
                 frameIdx1, aus[0], aus[1], aus[2], aus[3], \
                            aus[4], aus[5], aus[6], aus[7], aus[8], aus[9]= line1.split()
-                aus = aus.astype(np.int)
-                line2 = all_samples[i+stride]
-                frameIdx2, _, _, _, _, \
-                           _, _, _, _, _, _, = line2.split()
-                print(frameIdx1, frameIdx2, aus[0], aus[1], aus[2], aus[3], \
-                           aus[4], aus[5], aus[6], aus[7], aus[8], aus[9], file=data)
+                    for j in range(1, stride):
+                    aus = aus.astype(np.int)
+                    line2 = all_samples[i+j]
+                    frameIdx2, _, _, _, _, \
+                            _, _, _, _, _, _, = line2.split()
+                    print(frameIdx1, frameIdx2, aus[0], aus[1], aus[2], aus[3], \
+                            aus[4], aus[5], aus[6], aus[7], aus[8], aus[9], file=data)
     data.close()
 
 if __name__ == '__main__':
