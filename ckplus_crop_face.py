d220fbcc900feac978c8d9d5af137164b4393ffe
alexandmrwh <wuhancs@pku.edu.cn>
2019-07-13 13:53:36 +0800

    add non png file processing

--- ckplus_crop_face.py
+++ ckplus_crop_face.py
@@ -16,48 +16,57 @@ INT_MAX = sys.maxsize
 INT_MIN = -sys.maxsize-1
 
 
-def all_label_process():
+def process():
     for SubIdx in range(1000):
         SubImagePath = CKPlusImagePath+'S'+str(SubIdx).zfill(3)+'/'
         # for each existed subject
-        if os.path.isdir(SubLabelPath):
+        if os.path.isdir(SubImagePath):
+            print(SubImagePath)
             # for each sequence
             for SeqIdx in range(20):
-            	left, top, right, bottom = 0.0, 0.0, 0.0, 0.0
-            	minx, miny, maxx, maxy = INT_MAX, INT_MAX, INT_MIN, INT_MIN
-            	SeqImagePath = SubImagePath+str(SeqIdx).zfill(3)+'/'
-            	if os.path.isdir(SeqImagePath):
-            		for framename in os.listdir(SeqImagePath):
-            		# detect face rect, return in [left, top, right, bottom]
-            			framepath = SeqImagePath + framename
-            			frame = cv2.imread(framepath)
-            			frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            			faceDetector = dlib.get_frontal_face_detector()
-            			face = faceDetector(frame_gray, 1)
-            			if len(face) == 0:
-					print("No face detected in frame{}".format(t))
-					continue
-				left, top, right, bottom = face[0].left(), face[0].top(), face[0].right(), face[0].bottom()
+                left, top, right, bottom = 0.0, 0.0, 0.0, 0.0
+                minx, miny, maxx, maxy = INT_MAX, INT_MAX, INT_MIN, INT_MIN
+                SeqImagePath = SubImagePath+str(SeqIdx).zfill(3)+'/'
+                print(SeqImagePath)
+                if os.path.isdir(SeqImagePath):
+                    for framename in os.listdir(SeqImagePath):
+                    # detect face rect, return in [left, top, right, bottom]
+			if os.path.splitext(framename)[-1][1:] != 'png':
+				continue 
+                        framepath = SeqImagePath + framename
+                        print(framepath)
+                        frame = cv2.imread(framepath)
+                        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                        faceDetector = dlib.get_frontal_face_detector()
+                        face = faceDetector(frame_gray, 1)
+                        if len(face) == 0:
+                            print("No face detected in frame{}".format(t))
+                            continue
 
-        			if left < minx:
-					minx = left
-        			if top < miny:
-        				miny = top
-    				if right > maxx:
-					maxx = right
-				if bottom > maxy:
-					maxy = bottom
+                        left, top, right, bottom = face[0].left(), face[0].top(), face[0].right(), face[0].bottom()
+                        if left < minx:
+                            minx = left
+                        if top < miny:
+                            miny = top
+                        if right > maxx:
+                            maxx = right
+                        if bottom > maxy:
+                            maxy = bottom
 
-			for framename in os.listdir(SeqImagePath):
-				framepath = SeqImagePath + framename
-				saveImagePath = CKPlusNewImagePath+'S'+str(SubIdx).zfill(3)+'/'+framename
-				print("saving ", saveImagePath)
-				frame = cv2.imread(framepath)
-				cv2.imwrite(saveImagePath,frame[miny: maxy, minx: maxx])
+                    for framename in os.listdir(SeqImagePath):
+                        framepath = SeqImagePath + framename
+                        saveImagefolder = CKPlusNewImagePath+'S'+str(SubIdx).zfill(3)+'/'+str(SeqIdx).zfill(3)
+                        if not os.path.isdir(saveImagefolder):
+                            os.makedirs(saveImagefolder)
+                        saveImagePath = saveImagefolder + '/' + framename
+                        print("saving ", saveImagePath)
+                        frame = cv2.imread(framepath)
+                        cv2.imwrite(saveImagePath,frame[miny: maxy, minx: maxx])
 
 
 
 
 if __name__ == '__main__':
 	process()
+
 
